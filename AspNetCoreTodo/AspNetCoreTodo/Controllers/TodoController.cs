a5788734f61f20d9852013b445ec362ac73f6605
rramos <rodrigohernan.ramos@gmail.com>
2022-05-06 15:16:41 -0300

    steep 4.3

---
+++ AspNetCoreTodo/AspNetCoreTodo/Controllers/TodoController.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AspNetCoreTodo.Services;
+using AspNetCoreTodo.Models;
+
+namespace AspNetCoreTodo.Controllers
+{
+    public class TodoController : Controller
+    {
+        private readonly ITodoItemService _todoItemService;
+
+        public TodoController(ITodoItemService todoItemService)
+        {
+            _todoItemService = todoItemService;
+        }
+
+        public IActionResult Index()
+        {
+            var items = _todoItemService.GetIncompleteItemsAsync();
+            var model = new TodoViewModel(){
+                Items = items
+            };
+
+            return View(model);
+        }
+    }
+}
