f11e771a2632a13dd38a3c6fc9c13403fa23754b
rramos <rodrigohernan.ramos@gmail.com>
2022-05-14 19:05:00 -0300

    version 1

---
+++ Inmobiliaria/Controllers/PropertyController.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Inmobiliaria.Models;
+
+namespace Inmobiliaria.Controllers;
+
+public class PropertyController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+}
