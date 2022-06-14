db72550acf50f6a6b0cf2d4314b838fe5e266300
rramos <rodrigohernan.ramos@gmail.com>
2022-06-14 15:59:57 -0300

    more weed

--- scripts/install-dev.sh
+++ scripts/install-dev.sh
@@ -1,1 +1,8 @@
-echo instalando base de datos
+#!/bin/bash
+
+################################################################################
+## Instalando dotnet
+################################################################################
+echo instalando dotnet
+sudo snap install dotnet-sdk --classic --channel=6.0
+sudo snap alias dotnet-sdk.dotnet dotnet
